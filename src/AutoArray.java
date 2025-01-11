38c60e8f56bd34ea40d8fc62dce3fa4598bfb549
nlysenko <nlysenko@national-platform.ru>
2025-01-11 22:30:56 +0300

    Training

---
+++ src/AutoArray.java
@@ -0,0 +1,7 @@
+// Улучшенная версия предыдущей программы (Array()).
+public class AutoArray {
+    public static void main(String[] args) {
+        int[] mounth_days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("В апреле " + mounth_days[3] + " дней.");
+    }
+}
