c5923c27e74d0280cb1bcfdb9fbef0c8c6d02ca3
nlysenko <nlysenko@national-platform.ru>
2025-01-11 21:12:28 +0300

    Training

---
+++ src/CharDemo2.java
@@ -0,0 +1,11 @@
+// Переменные cher ведут себя подобно целым чмслам.
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 содержит " + ch1);
+        ch1++; // Инекрементировать ch1
+        System.out.println("ch1 теперь содержит " + ch1);
+    }
+}
